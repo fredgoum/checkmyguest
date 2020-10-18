0c57b36741529436aadcfe4214186fd2dac470bd
fredgoum <alfredgoumou@gmail.com>
2020-10-18 16:30:01 +0200

    fix contact nav border bottom

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
     <!-- Home Navigation -->
-    <v-app-bar class="nav" v-if="homePage" app flat height="50" style="background: #ffffff; border-bottom: 4px solid #d4d1cc !important;">
+    <v-app-bar id="home-nav" v-if="homePage" app flat height="54" style="background: #ffffff; border-bottom: 4px solid #d4d1cc !important;">
       <img class="mx-auto my-2" src="@/assets/logo-checkmyguest.png" alt="checkmyguest logo" width="40">
     </v-app-bar>
     <!-- Contacts Navigation -->
-    <v-app-bar class="nav" v-else app flat height="50" style="background: #ffffff; border-bottom: 4px solid #003a83 !important;">
-      <div style="font-size: 20px; cursor: pointer;" @click="goToHome()">
+    <v-app-bar id="contact-nav" v-else app flat height="54" style="background: #ffffff;">
+      <div style="font-size: 5px; cursor: pointer;" @click="goToHome()">
         <v-icon class="fas fa-arrow-left"></v-icon>
       </div>
       <v-spacer></v-spacer>
@@ -25,6 +25,16 @@
     </v-app-bar>
   </div>
 </template>
+
+<style>
+  #contact-nav::before {
+    content: '';
+    position: absolute;
+    bottom: 0;
+    width: 65%;
+    border-bottom: 4px solid #003a83 !important;
+  }
+</style>
 
 <script>
   export default {
